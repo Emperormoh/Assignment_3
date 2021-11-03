0a32304b52e0deb2a1495cbcb6faa63728b24f14
Suleiman Muhammed Bello <wemabank@Wemas-MacBook-Pro-2.local>
2021-11-03 09:45:49 +0100

    initial commit

---
+++ Bank.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3
+{
+    public class Bank
+    {
+        private double balance = 20000;
+        public double accessedBalance
+        {
+            get { return balance; }
+            set { balance = value; }
+
+        }
+    }
+
+    class NewBank
+    {
+        Bank Suleiman = new Bank();
+        string name;
+        int account;
+        double withdraw;
+        double deposit;
+        double total;
+
+
+        public void ReceiveMoney()
+        {
+            Console.WriteLine("\nWelcome to Suleiman Bank.", "\nEnter name of depositior");
+            name = Console.ReadLine();
+            Console.WriteLine("Please Enter your account number");
+            account = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please input the amount you want to deposit");
+            deposit = double.Parse(Console.ReadLine());
+            total = Suleiman.accessedBalance + deposit;
+
+
+            Console.WriteLine($"Total Balance is now ${total}");
+
+
+        }
+
+        public void SendMoney()
+        {
+            Console.WriteLine("please enter your account number");
+            account = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the amount you want to withdraw");
+            withdraw = double.Parse(Console.ReadLine());
+
+            if (withdraw <= Suleiman.accessedBalance)
+            {
+                total = Suleiman.accessedBalance - withdraw;
+                Console.WriteLine("your new balance is :" + total);
+                
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+
+            }
+
+
+        }
+        public void checkBalance()
+        {
+            Console.WriteLine("your account balance is :" + total);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char run;
+
+            do
+            {
+                NewBank Wema = new NewBank();
+                int num;
+                Console.WriteLine("Please Select an Action");
+                Console.WriteLine("\nFor Deposit, Press 1");
+                Console.WriteLine("\nFor withdrawal, Press 2");
+                Console.WriteLine("\nFor baalance, Press 3");
+
+                num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Wema.ReceiveMoney();
+                        break;
+                    case 2:
+                        Wema.SendMoney();
+                        break;
+                    case 3:
+                        Wema.checkBalance();
+                        break;
+                    default:
+
+                        Console.WriteLine("Invalid Isnput!");
+                        break;
+
+
+                }
+                Console.WriteLine("\nDo you want to carry out another transaction?. (y/n)");
+                run = char.Parse(Console.ReadLine());
+
+            }
+            while (run == 'y');
+            {
+                Console.ReadKey();
+
+            }
+        }
+    }
+   
+}
